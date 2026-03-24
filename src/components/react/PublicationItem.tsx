f30505a3eb0187b8c352dd13486ff603706d6bea
qingxinxia <xiqxin@outlook.com>
2026-03-24 10:02:06 +0900

    news+publication format

--- src/components/react/PublicationItem.tsx
+++ src/components/react/PublicationItem.tsx
@@ -9,13 +9,13 @@ export const PublicationItem: React.FC<Props> = ({ pub }) => {
     return (
         <div className="py-3 border-b border-white/5 last:border-0 hover:bg-white/5 transition-colors rounded-lg px-4 -mx-4">
             <div className="text-gray-300 leading-relaxed text-sm lg:text-base">
-                <span className="font-medium text-white">{pub.authors}</span>: 
-                <span>{pub.title}</span>, 
+                <span className="font-medium text-white">{pub.authors}</span>{': '}
+                <span>{pub.title}</span>{', '}
                 <span className="italic">{pub.journal}</span>
-                {pub.volume && <span>, {pub.volume}</span>}
+                {pub.volume && <span>{', '}{pub.volume}</span>}
                 {pub.issue && <span>{pub.issue}</span>}
-                {pub.pages && <span>, {pub.pages}</span>} 
-                <span> {pub.dateDisplay}.</span>
+                {pub.pages && <span>{', '}{pub.pages}</span>}
+                {' '}<span>{pub.dateDisplay}.</span>
                 
                 {(pub.doi || pub.url) && (
                     <span className="inline-flex gap-2 ml-2">
